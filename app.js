23e07c53f94e21bb8c9558556296e2a996be0039
Érik Marconato <erik.marconato@hotmail.com>
2025-01-17 15:37:29 -0300

    feat: added raffle system

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaNomes = [];
+
+function adicionarAmigo(){
+
+    nomeAmigo = document.querySelector("#amigo").value;
+
+    if (nomeAmigo.trim() === ""){
+        alert("Por favor, digite um nome válido!")
+        return
+    }
+
+    listaNomes.push(nomeAmigo);
+        document.querySelector("#amigo").value = "";
+        atualizarLista();
+}
+
+function atualizarLista(){
+
+    const lista = document.querySelector("#listaAmigos");
+
+    lista.innerHTML = "";
+
+    listaNomes.forEach((nome) => {
+        const item = document.createElement("li");
+        item.textContent = nome;
+        lista.appendChild(item);
+    })
+}
+
+function sortearAmigo(){
+    
+    if (listaNomes.length === 0){
+        alert("Lista vazia. Por favor adicione amigos antes de realizar o sorteio.");
+        return
+    }
+
+    const sorteio = Math.floor(Math.random() * listaNomes.length);
+
+    const nomeSorteado = listaNomes[sorteio];
+
+    document.querySelector("#resultado").innerHTML = nomeSorteado;
+}
+
